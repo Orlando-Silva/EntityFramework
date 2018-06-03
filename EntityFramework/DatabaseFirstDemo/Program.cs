61db07d704498dffcbf093f1925bba6d3e75d0a3
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-06-03 15:48:14 -0300

    Demostração do approach Database First

---
+++ EntityFramework/DatabaseFirstDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uso rápido:
+            var contexto = new DatabaseFirstDemoEntities();
+            var post = new Post()
+            {
+                Body = "Hello World!",
+                DatePublished = DateTime.UtcNow,
+                Title = "Database Hello World"
+            };
+            contexto.Post.Add(post); // Aqui é o "Commit".
+            contexto.SaveChanges(); // Aqui é onde realmente é salvo, o "Push".
+
+        }
+    }
+}
